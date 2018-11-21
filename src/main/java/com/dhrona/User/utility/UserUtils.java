9fd930a97941de5d066c0b992a6c533fc6058692
Dhrona <MSDhrona7@gmail.com>
2018-11-21 19:32:04 +0530

    password hashing

---
+++ src/main/java/com/dhrona/User/utility/UserUtils.java
@@ -0,0 +1,15 @@
+package com.dhrona.User.utility;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class UserUtils {
+    public static String hashPassword(String password) {
+        return  BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+    public static boolean checkPassWord(String password, String hashedPassword) {
+        boolean checkPwd = false;
+        if (BCrypt.checkpw(password, hashedPassword)) checkPwd = true;
+            return checkPwd;
+    }
+
+}
