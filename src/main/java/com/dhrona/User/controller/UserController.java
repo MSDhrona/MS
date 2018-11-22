58193a9b57d0b5d5b36839bfcfaa9b10c937e302
Dhrona <MSDhrona7@gmail.com>
2018-11-22 14:37:17 +0530

    Success Response

--- src/main/java/com/dhrona/User/controller/UserController.java
+++ src/main/java/com/dhrona/User/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.dhrona.User.controller;
 
+import com.dhrona.User.bean.ExceptionResponse;
 import com.dhrona.User.bean.UserRegistration;
 import com.dhrona.User.entity.UserDetails;
 import com.dhrona.User.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -17,10 +19,11 @@ public class UserController {
     public String welcome(){
         return "welcome to dhrona MS session";
     }
+
     @PostMapping("/")
-    @ResponseStatus(HttpStatus.CREATED)
-    public UserDetails register(@Valid @RequestBody UserRegistration registrationDetails) throws Exception {
-        return userService.register(registrationDetails);
+    public ResponseEntity<String> register(@Valid @RequestBody UserRegistration registrationDetails) throws Exception {
+            userService.register(registrationDetails);
+            return new ResponseEntity<String>("User Created SuccessFully", HttpStatus.CREATED);
     }
 
 
